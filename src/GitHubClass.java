d5f778c069dbb03a7e843377da4545d53e354136
Nasratulhaq <nasrtulhaq.fazli@tekschool.us>
2022-05-11 12:45:40 -0600

    this is the new class

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -1,4 +1,12 @@
 
 public class GitHubClass {
+	public static void main(String[] args) {
+		System.out.println("Hello This is Git Practice");
+		
+		int a=10;
+		int b=15;
+		int sum=a+b;
+		System.out.println(sum);
+	}
 
 }
